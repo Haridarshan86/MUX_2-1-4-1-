ba9570b78dcd2f4d35e43c17e100af1d5d3db827
Haridarshan B Kunder <darshanhari86@gmail.com>
2026-06-17 20:03:59 +0530

    Add files via upload

---
+++ mux4x1.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.06.2026 20:01:37
+// Design Name: 
+// Module Name: mux4x1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+module mux4x1(input i0,input i1,input i2,input i3,input s0,input s1,output reg y);
+
+always @(*)
+begin
+case({s1,s0})
+        2'b00: y = i0;
+        2'b01: y = i1;
+        2'b10: y = i2;
+        2'b11: y = i3;
+endcase
+end
+endmodule
